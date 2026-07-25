13755667d45a2cb976fdc1bdb58bb429d25f703c
Omar-afqir <66503583+Omar-afqir@users.noreply.github.com>
2026-07-25 11:54:13 +0100

    Update load_bronze.sql

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -1,1 +1,83 @@
+--- bronze crm_cust_info
+TRUNCATE TABLE bronze.crm_cust_info;
 
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\u31t02\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+
+
+--- bronze crm_prd_info
+TRUNCATE TABLE bronze.crm_prd_info;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\u31t02\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+
+
+
+--- bronze crm_sales_details
+TRUNCATE TABLE bronze.crm_sales_details;
+
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\u31t02\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+
+--- bronze erp_cust_az12
+TRUNCATE TABLE bronze.erp_cust_az12;
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\u31t02\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+
+
+--- bronze erp_loc_a101
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\u31t02\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+
+
+--- bronze erp_loc_a101
+TRUNCATE TABLE bronze.erp_px_cat_g1V2;
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\u31t02\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+GO
+
+SELECT * FROM bronze.erp_px_cat_g1v2;
